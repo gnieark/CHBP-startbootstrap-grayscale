ab1039692d35167c9807572aef74935c793c8e1d
Gnieark <rpasserieu@ch-boispetit.fr>
2015-06-29 11:18:15 +0200

    add dotclear php files

---
+++ _define.php
@@ -0,0 +1,23 @@
+<?php
+# -- BEGIN LICENSE BLOCK ---------------------------------------
+# This file is part of Grayscale, a theme for Dotclear
+#
+# Copyright (c) 2015 - Association Dotclear
+# Licensed under the GPL version 2.0 license.
+# See LICENSE file or
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK -----------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */				"CHBP",
+	/* Description*/		"personalisation du thème \"Simple Grayscale Bootstrap theme for Dotclear\" pour le Centre Hospitalier du Bois Petit",
+	/* Author */			"Centre Hospitalier du Bois Petit - 76300 Sotteville-les-Rouen",
+	/* Version */			'0.1',
+	/* Properties */		array(
+								'type' => 'theme',
+								'tplset' => 'mustek'
+							)
+);
